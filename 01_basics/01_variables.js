96f072e6327d86e28f548c3d7303936263a3ff4f
SrinivaasGudur07 <srinivaasguduru@gmail.com>
2026-06-02 13:11:40 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 1234;
+let accountEmail = "srinivaas@gmail.com";
+var accountPassword = "12321";
+
+accountCity = "Nellore";
+
+//accountId = 23; can not modify the const variable once it is decalred.
+
+//console.log(accountId);
+
+accountEmail = "guduru@gmail.com";
+
+accountPassword = "hello";
+
+accountCity = "NLR";
+
+console.log(accountCity);
+
+console.log(accountPassword);
+
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountPassword,accountCity])
